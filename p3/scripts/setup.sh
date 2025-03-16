23cd705e28dd33272882518beb3d75613671da95
danto29 <dantol29@gmail.com>
2025-03-16 17:09:32 +0200

    improve script

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 set -e
 
+# Installation for Mac Silicon chips
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
 k3d cluster delete --all
 
+if [ $? -eq 127 ]; then
+    echo -e "${GREEN} Installing k3d ${RESET}"
+    curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+    k3d cluster delete --all
+fi
+
 k3d cluster create --config ../confs/conf.yml
 
 k3d kubeconfig get dtolmaco > /dev/null
 
 kubectl config use-context k3d-dtolmaco
+
+if [ $? -eq 127 ]; then
+    echo -e "${GREEN} Installing kubectl${RESET}"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl"   
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl config use-context k3d-dtolmaco
+fi
 
 if [ "$(kubectl config current-context)" = "k3d-dtolmaco" ]; then
     echo -e "${GREEN}Context initialized successfully${RESET}"
@@ -34,6 +49,6 @@ fi
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-sleep 30
+sleep 5
 
 kubectl get pods -n argocd
