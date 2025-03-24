08380ffcbecad90e96eac3364a85339f32bfc60f
Jules <jbjulesbernard3@gmail.com>
2025-03-24 19:53:41 +0100

    while wait info print optimized

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -36,9 +36,9 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # Wait for ArgoCD pods to be ready
 echo -e "${GREEN} Waiting for ArgoCD to be ready... ${RESET}"
 while ! kubectl get pods -n argocd | grep "argocd-server" | grep -q "Running"; do
-    kubectl get pods -n argocd
     echo -e "${RED} ArgoCD is not ready yet... ${RESET}"
     sleep 10
+	kubectl get pods -n argocd
 done
 echo -e "${GREEN} ArgoCD is ready! ${RESET}"
 
@@ -61,9 +61,9 @@ done
 # Wait for the app to be ready
 echo -e "${GREEN} Waiting for the app to be deployed... ${RESET}"
 while ! kubectl get pods -n dev | grep "dtolmaco-42" | grep -q "Running"; do
-    kubectl get pods -n dev
     echo -e "${RED} App is not ready yet... ${RESET}"
     sleep 10
+	kubectl get pods -n dev
 done
 
 NAME=$(kubectl get pods -n dev -o custom-columns="NAME:.metadata.name" | grep "dtolmaco-42" | head -n 1)
