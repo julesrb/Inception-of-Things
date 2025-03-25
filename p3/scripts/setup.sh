aa0411efc23911e22fe44e5711ab3c4217125356
Jules <jbjulesbernard3@gmail.com>
2025-03-25 14:06:55 +0100

    Revert "Merge branch 'fix-7' of github.com:julesrb/Inception-of-Things into jules-bonus"
    This reverts commit 0b9e68508bb3320f7f3a3b68da624c7936ce0656, reversing
    changes made to 80fed6dc0add41e35191395f2170cc23d9d7d530.

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -17,9 +17,7 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
-#kill previous k3s instanced and port forwarding
-k3d cluster delete --all || true
-pkill -f "kubectl port-forward" || true
+k3d cluster delete --all
 
 k3d cluster create --config ../conf.yml
 
@@ -33,19 +31,14 @@ kubectl create namespace dev
 # Create an Argo CD app in argocd namespace
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null
 
-# Wait for ArgoCD pods to be ready
-echo -e "${GREEN} Waiting for ArgoCD to be ready... ${RESET}"
-while ! kubectl get pods -n argocd | grep "argocd-server" | grep -q "Running"; do
-	echo -e "${RED} ArgoCD is not ready yet... ${RESET}"
-	sleep 10
-	kubectl get pods -n argocd
-done
-echo -e "${GREEN} ArgoCD is ready! ${RESET}"
+# give time for argocd to load
+echo -e "${GREEN} Sleeping for 40 seconds ${RESET}"
+sleep 40
 
 kubectl get pods -n argocd
 
 # forward ports to access argocd from localhost:8080
-nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
 PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
 echo -e "${GREEN} Password is ${PASSWORD} ${RESET}"
@@ -53,20 +46,11 @@ echo -e "${GREEN} Password is ${PASSWORD} ${RESET}"
 argocd login localhost:8080 --username admin --password ${PASSWORD} --insecure
 
 # deploy an app from our git
-while ! argocd app create dtolmaco-42 --repo https://github.com/julesrb/Inception-of-Things.git --revision dtolmaco/p3 --path p3/confs --dest-server https://kubernetes.default.svc --dest-namespace dev --sync-policy auto; do
-	echo "App deployment failed. Retrying..."
-	sleep 5
-done
+argocd app create dtolmaco-42 --repo https://github.com/julesrb/Inception-of-Things.git --revision dtolmaco/p3 --path p3/confs --dest-server https://kubernetes.default.svc --dest-namespace dev --sync-policy auto
 
-# Wait for the app to be ready
-echo -e "${GREEN} Waiting for the app to be deployed... ${RESET}"
-while ! kubectl get pods -n dev | grep "dtolmaco-42" | grep -q "Running"; do
-	echo -e "${RED} App is not ready yet... ${RESET}"
-	sleep 10
-	kubectl get pods -n dev
-done
+sleep 10
 
 NAME=$(kubectl get pods -n dev -o custom-columns="NAME:.metadata.name" | grep "dtolmaco-42" | head -n 1)
 
 # forward ports to access deployed app from localhost:8888
-nohup kubectl port-forward pod/${NAME} 8888:8888 -n dev > /dev/null 2>&1 &
+kubectl port-forward pod/${NAME} 8888:8888 -n dev &
