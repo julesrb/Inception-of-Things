130141443fe1a4a6c058dab056b82b5d498c67ae
Jules <jbjulesbernard3@gmail.com>
2025-03-26 13:47:15 +0100

    reset gitlab install

--- bonus/scripts/gitlab.sh
+++ bonus/scripts/gitlab.sh
@@ -5,10 +5,17 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
+# Reset
+helm uninstall gitlab -n gitlab
+kubectl delete all --all -n gitlab
+kubectl delete namespace gitlab
+
+
 # Installing helm 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
 
 # Installing GitLab
 kubectl create namespace gitlab
