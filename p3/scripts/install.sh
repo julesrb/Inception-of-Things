3673ebd834d42df96286a117bce69547e95296dd
danto29 <dantol29@gmail.com>
2025-05-26 15:48:39 +0300

    add argocd to installation

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -31,4 +31,8 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 echo "Installing the auto-completion scripts for k3d"
 echo "source <(k3d completion bash)" >> ~/.bashrc
 
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
 echo "..::OK::.."
